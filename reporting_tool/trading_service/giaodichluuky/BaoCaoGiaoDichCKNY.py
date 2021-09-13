610f57f33d756934141b68f9cb919754ec1a842c
phs.vn <hiepdang@phs.vn>
2021-09-13 09:54:09 +0700

    add BaoCaoGiaoDichCKNY

--- reporting_tool/trading_service/giaodichluuky/BaoCaoGiaoDichCKNY.py
+++ reporting_tool/trading_service/giaodichluuky/BaoCaoGiaoDichCKNY.py
@@ -16,7 +16,7 @@ def run(
     if not os.path.isdir(join(dept_folder, folder_name, period)):  # dept_folder from import
         os.mkdir(join(dept_folder, folder_name, period))
 
-    trading_record = pd.read_sql(
+    ytd_trading_record = pd.read_sql(
         "SELECT sub_account, exchange, type_of_account, type_of_asset, type_of_order, value "
         "FROM trading_record "
         f"WHERE date BETWEEN '{begin_of_year}' AND '{end_date}' "
@@ -24,38 +24,211 @@ def run(
         connect,
         index_col='sub_account',
     )
-    account = pd.read_sql(
-        "SELECT sub_account, account_code FROM sub_account;",
+    period_trading_record = pd.read_sql(
+        "SELECT sub_account, exchange, type_of_account, type_of_asset, type_of_order, value "
+        "FROM trading_record "
+        f"WHERE date BETWEEN '{start_date}' AND '{end_date}' "
+        f"AND settlement_period IN (1,2);",
         connect,
         index_col='sub_account',
-    ).squeeze()
-    broker = pd.read_sql(
-        "SELECT account_code, broker_id FROM account",
-        connect,
-        index_col='account_code',
-    ).squeeze()
-    branch_id = pd.read_sql(
-        "SELECT broker_id, branch_id FROM broker",
-        connect,
-        index_col='broker_id',
-    ).squeeze()
-    branch_name = pd.read_sql(
-        "SELECT branch_id, branch_name FROM branch;",
-        connect,
-        index_col='branch_id',
-    ).squeeze()
-
-    trading_record['exchange'].replace('UPCOM','HNX',inplace=True)
-    domestic = trading_record['type_of_account'].str.endswith('trong nước')
-    foreign = trading_record['type_of_account'].str.endswith('nước ngoài')
-    tudoanh = trading_record['type_of_account'].inna() # chờ IT fix loi se sua thanh .str.endswith('tu doanh')
-    trading_record.loc[domestic] = 'trong nước'
-    trading_record.loc[foreign] = 'nước ngoài'
-    trading_record.loc[tudoanh] = 'tự doanh'
-    trading_record.insert(
-        3,
-        'type',
-        trading_record['type_of_account']+' - '+trading_record['type_of_asset']
     )
 
+    def f(df,ttype):
+        df['exchange'].replace('UPCOM','HNX',inplace=True)
+        df['exchange'].replace('HOSE','HSX', inplace=True)
+        df['type_of_account'].fillna('Tự doanh',inplace=True) # cho IT fill luon tren DB thi bo dong nay di
+        domestic = df['type_of_account'].str.endswith('trong nước')
+        foreign = df['type_of_account'].str.endswith('nước ngoài')
+        tudoanh = df['type_of_account'].str.endswith('Tự doanh')
+        df.loc[domestic,'type_of_account'] = 'domestic'
+        df.loc[foreign,'type_of_account'] = 'foreign'
+        df.loc[tudoanh,'type_of_account'] = 'dealing'
+        df['type_of_asset'] = df['type_of_asset'].map(
+            {
+                'Cổ phiếu thường': 'stock',
+                'Chứng chỉ quỹ': 'fund_certificate',
+                'Chứng quyền': 'cw',
+                'Trái phiếu': 'bond',
+            }
+        )
+        result = df.groupby(['type_of_order','exchange','type_of_asset','type_of_account']).sum()
+        if ttype == 'ytd':  result.columns = pd.Index(['value_ytd'],name='type_of_period')
+        elif ttype == 'period': result.columns = pd.Index(['value_period'],name='type_of_period')
+        else: raise TypeError("ttpe only accepts 'ytd' or 'period'")
+        return result
 
+    ytd_table = f(ytd_trading_record,'ytd')
+    period_table = f(period_trading_record,'period')
+    result = pd.concat([period_table,ytd_table],axis=1)
+    result = result.unstack(['type_of_order','exchange'])
+    result = result.reorder_levels(['type_of_order','type_of_period','exchange'],axis=1)
+    result.fillna(0,inplace=True)
+    needed_idx = pd.MultiIndex.from_tuples([
+        ('stock','domestic'),
+        ('stock','foreign'),
+        ('fund_certificate','domestic'),
+        ('fund_certificate','foreign'),
+        ('stock','dealing'),
+        ('bond','dealing'),
+        ('fund_certificate','dealing'),
+    ], names=['type_of_asset','type_of_account'])
+    result = pd.DataFrame(data=0,columns=result.columns,index=needed_idx).add(result,fill_value=0) # ensure complete index
+    result[('T','value_period','HSX')] = result[('B','value_period','HSX')] + result[('S','value_period','HSX')]
+    result[('T','value_period','HNX')] = result[('B','value_period','HNX')] + result[('S','value_period','HNX')]
+    result[('T','value_ytd','HSX')] = result[('B','value_ytd','HSX')] + result[('S','value_ytd','HSX')]
+    result[('T','value_ytd','HNX')] = result[('B','value_ytd','HNX')] + result[('S','value_ytd','HNX')]
+    cols = [
+        ('B','value_period','HNX'),
+        ('B','value_period','HSX'),
+        ('B','value_ytd','HNX'),
+        ('B','value_ytd','HSX'),
+        ('S','value_period','HNX'),
+        ('S','value_period','HSX'),
+        ('S','value_ytd','HNX'),
+        ('S','value_ytd','HSX'),
+        ('T','value_period','HNX'),
+        ('T','value_period','HSX'),
+        ('T','value_ytd','HNX'),
+        ('T','value_ytd','HSX'),
+    ]
+    result = result[cols] # ensure column order matched with excel output
+
+    # Write to Báo cáo phí chuyển khoản
+    file_name = f'Báo cáo tình hình HĐKD (Biểu II.6) {period}.xlsx'
+    writer = pd.ExcelWriter(
+        join(dept_folder,folder_name,period,file_name),
+        engine='xlsxwriter',
+        engine_kwargs={'options':{'nan_inf_to_errors':True}}
+    )
+    workbook = writer.book
+    worksheet = workbook.add_worksheet(period)
+    worksheet.hide_gridlines(option=2)
+    # set column width
+    worksheet.set_column('A:A',8)
+    worksheet.set_column('B:B',16)
+    worksheet.set_column('C:N',11.5)
+    worksheet.set_row(7,47)
+    worksheet.set_row(8,47)
+    worksheet.set_row(9,47)
+    worksheet.set_row(10,47)
+    title_format = workbook.add_format(
+        {
+            'bold': True,
+            'font_name': 'Arial',
+            'align': 'center',
+        }
+    )
+    dvt_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'italic': True,
+            'align': 'right',
+        }
+    )
+    supheader_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'bold': True,
+            'align': 'center',
+            'valign': 'vcenter',
+            'border': 1,
+        }
+    )
+    header_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'align': 'center',
+            'valign': 'vcenter',
+            'border': 1,
+        }
+    )
+    stt_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'align': 'center',
+            'valign': 'top',
+            'border': 1,
+        }
+    )
+    index_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'text_wrap': True,
+            'valign': 'top',
+            'border': 1
+        }
+    )
+    supindex_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'bold': True,
+            'text_wrap': True,
+            'valign': 'top',
+            'border': 1
+        }
+    )
+    value_format = workbook.add_format(
+        {
+            'font_name': 'Times New Roman',
+            'num_format': '_(* #,##0_);_(* (#,##0);_(* "-"??_);_(@_)',
+            'border': 1
+        }
+    )
+    worksheet.merge_range('A1:N1',f'Báo cáo tình hình HĐKD - Biểu II.6 - {period}',title_format)
+    worksheet.merge_range('A3:A5','STT',header_format)
+    worksheet.merge_range('B3:B5','Loại chứng khoán',header_format)
+    worksheet.merge_range('C3:F3','Tổng mua',supheader_format)
+    worksheet.merge_range('G3:J3','Tổng bán',supheader_format)
+    worksheet.merge_range('K3:N3','Tổng mua + bán',supheader_format)
+    worksheet.merge_range('C4:D4','Trong kỳ',header_format)
+    worksheet.merge_range('E4:F4','Lũy kế từ đầu năm',header_format)
+    worksheet.merge_range('G4:H4','Trong kỳ',header_format)
+    worksheet.merge_range('I4:J4','Lũy kế từ đầu năm',header_format)
+    worksheet.merge_range('K4:L4','Trong kỳ',header_format)
+    worksheet.merge_range('M4:N4','Lũy kế từ đầu năm',header_format)
+    worksheet.write_row('C5',['HNX','HSX']*6,header_format)
+    worksheet.write_row('A6',np.arange(14)+1,header_format)
+    worksheet.write_column('A7',np.arange(9)+1,stt_format)
+    idx_list = [
+        'A. Nhà đầu tư',
+        '1. Giao dịch cổ phiếu của nhà đầu tư trong nước',
+        '2. Giao dịch cổ phiếu của nhà đầu tư nước ngoài',
+        '3. Giao dịch chứng chỉ quỹ của nhà đầu tư trong nước',
+        '4. Giao dịch chứng chỉ quỹ của nhà đầu tư nước ngoài',
+        'B. Tự doanh',
+        '1. Cổ phiếu',
+        '2. Trái phiếu',
+        '3. Chứng chỉ quỹ',
+    ]
+    for row, idx in enumerate(idx_list):
+        if idx in ['A. Nhà đầu tư','B. Tự doanh']:
+            fmt = supindex_format
+        else:
+            fmt = index_format
+        worksheet.write(row+6,1,idx,fmt)
+    worksheet.write_row('C7',[None]*12,value_format)
+    worksheet.write_row('C12',[None]*12,value_format)
+    dvt = 'mil'
+    if dvt=='bil': div = 1e9 ; dvt_text = 'Đơn vị tính: tỷ đồng'
+    elif dvt=='mil': div = 1e6 ; dvt_text = 'Đơn vị tính: triệu đồng'
+    elif dvt=='k': div = 1e3 ; dvt_text = 'Đơn vị tính: nghìn đồng'
+    elif dvt=='unit': div = 1 ; dvt_text = 'Đơn vị tính: đồng'
+    else: raise ValueError("dvt must be either 'bil', 'mil', 'k', 'unit'")
+    worksheet.merge_range('L2:N2',dvt_text,dvt_format)
+    worksheet.write_row('C8',result.loc[('stock','domestic')]/div, value_format)
+    worksheet.write_row('C9',result.loc[('stock','foreign')]/div, value_format)
+    worksheet.write_row('C10',result.loc[('fund_certificate','domestic')]/div, value_format)
+    worksheet.write_row('C11',result.loc[('fund_certificate','foreign')]/div, value_format)
+    worksheet.write_row('C13',result.loc[('stock','dealing')]/div, value_format)
+    worksheet.write_row('C14',result.loc[('bond','dealing')]/div, value_format)
+    worksheet.write_row('C15',result.loc[('fund_certificate','dealing')]/div, value_format)
+    writer.close()
+
+    # Write to word
+
+
+    if __name__ == '__main__':
+        print(f"{__file__.split('/')[-1].replace('.py','')}::: Finished")
+    else:
+        print(f"{__name__.split('.')[-1]} ::: Finished")
+    print(f'Total Run Time ::: {np.round(time.time()-start,1)}s')
