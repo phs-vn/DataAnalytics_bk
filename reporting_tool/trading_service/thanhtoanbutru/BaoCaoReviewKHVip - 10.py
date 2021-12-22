9c58ce1169126fc13a4e42393bfa0c716974dd32
Nam Tran Huy <namtran@phs.vn>
2021-12-22 17:51:46 +0700

    push-new-code-report-10-(1/2)

--- reporting_tool/trading_service/thanhtoanbutru/BaoCaoReviewKHVip - 10.py
+++ reporting_tool/trading_service/thanhtoanbutru/BaoCaoReviewKHVip - 10.py
@@ -1,15 +1,14 @@
 """
     1. Các cột G, H, I phải tính mới ra được kết quả. Tất cả đều có công thức trong file mẫu
-    2. Nếu là GOLD PHS và SILV PHS thì cứ 6 tháng set 1 lần (VD: 01/01/2021 tới 30/06/2021)
-       Nếu là VIP Branch thì 3 tháng set 1 lần (từ đầu quý trước tới cuối quý trước)
-       - Ví dụ báo cáo suất vào quý 3 của năm (30/9/2021) --> chỉ xét 3 tháng
-       hoặc dễ hiểu hơn là chỉ review VIP CN (từ 01/07 tới 30/09)
-       - Nếu báo cáo suất vào quý 2 hoặc quý 4 --> xét cả 3 tháng và 6 tháng
-       hoặc dễ hiểu hơn là review cả VIP CN, GOLD, SILV (
-            ví dụ quý 2: 30/06
-            --> xét 3 tháng: từ 01/04 tới 30/06, xét 6 tháng: từ 01/01/2021 tới 30/06/2021)
-            quý 4: 31/12
-            --> xét 3 tháng: từ 01/10 tới 31/12, xét 6 tháng: từ 01/07/2021 tới 31/12/2021)
+    2. Nguyên tắc là VIP CN thì 3 tháng review 1 lần chốt cuối mỗi quý thì từ thời gian chốt
+    đó tính ngược lại 3 tháng cứ thế mà đếm ra, còn GOLD và SILV thì sáu tháng tức là 1 năm 2 lần
+    cuối tháng 6 và cuối tháng 12 cứ thế mà đếm ngược lại.
+    Nguyên tắc thời gian, nếu báo cáo chạy vào thời điểm quý 2, quý 4 là sẽ xét toàn bộ KH (gold, vip, silv),
+    còn nếu chạy vào thời điểm quý 1, quý 3 thì chỉ xét KH VIP CN
+    3. Các cột M, N, O, P lấy ý kiến từ các phòng ban khác
+    4. Cột Note để trắng cho CN họ note cái gì thì note.
+       Cột MOI GIOI QUAN LY thì lên dữ liệu cho chị Tuyết
+    5. TK 022C036979 có contract type là 'MR - KHTN GOLD 70% - OD 0.15% - Margin. PIA 10% - MR 2 - DP 5 (Lãi suất)'
 """
 from reporting_tool.trading_service.thanhtoanbutru import *
 
@@ -20,8 +19,8 @@ def run(
 ):
     start = time.time()
     info = get_info('quarterly', run_time)
-    start_date = info['start_date']
-    end_date = info['end_date']
+    start_date = info['start_date'].replace('/', '-')
+    end_date = info['end_date'].replace('/', '-')
     period = info['period']
     folder_name = info['folder_name']
 
@@ -35,9 +34,10 @@ def run(
     ###################################################
     ###################################################
     ###################################################
-    # month of end_date
-    moe = dt.datetime.strptime(end_date, "%Y/%m/%d")
 
+    sod = start_date
+
+    # query danh sách khách hàng
     review_vip = pd.read_sql(
         f"""
         SELECT
@@ -84,7 +84,11 @@ def run(
         AND -- De chay baktest
             [account].[date_of_open] <= '{end_date}' 
         AND -- De chay baktest
-            ([account].[date_of_close] > '{end_date}' OR [account].[date_of_close] IS NULL OR [account].[date_of_close] = '2099-12-31')
+            (
+                [account].[date_of_close] > '{end_date}' 
+                OR [account].[date_of_close] IS NULL 
+                OR [account].[date_of_close] = '2099-12-31'
+            )
         AND 
             [customer_information_change].[change_content] = 'Loai hinh hop dong'
         ORDER BY
@@ -98,91 +102,102 @@ def run(
     # Groupby and condition
     review_vip['effective_date'] = review_vip.groupby('account_code')['date_of_change'].min()
     review_vip['approved_date'] = review_vip.groupby('account_code')['date_of_approval'].max()
-    mask = (
-                review_vip['date_of_change'] == review_vip['effective_date']
-           ) | (
-                review_vip['date_of_approval'] == review_vip['approved_date'])
+    mask = (review_vip['date_of_change']==review_vip['effective_date'])|(review_vip['date_of_approval']==review_vip['approved_date'])
     review_vip = review_vip.loc[mask]
 
     review_vip = review_vip[[
-        'birth_month',
         'customer_name',
         'branch_name',
-        'date_of_birth',
         'contract_type',
-        'broker_id',
         'broker_name',
-        'effective_date',
         'approved_date',
     ]]
+
+    review_vip['approved_date'] = pd.to_datetime(review_vip['approved_date']).dt.date
     # convert cột contract_type
-    review_vip['contract_type'] = review_vip['contract_type'].apply(
+    review_vip['current_vip'] = review_vip['contract_type'].apply(
         lambda x: 'SILV PHS' if 'SILV' in x else ('GOLD PHS' if 'GOLD' in x else 'VIP Branch')
     )
     review_vip.drop_duplicates(keep='last', inplace=True)
 
-    # điều kiện phân biệt giữa SILV, GOLD và VIP Branch
-    for idx in review_vip.index:
-        contract_type = review_vip.loc[idx, 'contract_type']
-        if contract_type == 'SILV PHS' or contract_type == 'GOLD PHS':
-            if 1 <= run_time.month < 6:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year, 6, 30).strftime("%d/%m/%Y")
-            elif 6 <= run_time.month < 12:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year, 12, 31).strftime("%d/%m/%Y")
-            else:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year+1, 6, 30).strftime("%d/%m/%Y")
-        elif contract_type == 'VIP Branch':
-            if 1 <= run_time.month < 3:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year, 3, 31).strftime("%d/%m/%Y")
-            elif 3 <= run_time.month < 6:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year, 6, 30).strftime("%d/%m/%Y")
-            elif 6 <= run_time.month < 9:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year, 9, 30).strftime("%d/%m/%Y")
-            else:
-                review_vip.loc[idx, 'review_date'] = dt.datetime(run_time.year+1, 3, 31).strftime("%d/%m/%Y")
-
-    branch_groupby_table = review_vip.groupby(['branch_name', 'contract_type'])['customer_name'].count().unstack()
-    branch_groupby_table.fillna(0, inplace=True)
-    review_vip['criteria_fee'] = review_vip['contract_type'].apply(
+    review_vip['criteria_fee'] = review_vip['current_vip'].apply(
         lambda x: 40000000 if ('GOLD' in x or 'VIP Branch' in x) else 20000000
     )
-    review_vip_phs = review_vip.loc[
-        (review_vip['contract_type'] == 'GOLD PHS')|(review_vip['contract_type'] == 'SILV PHS')].copy()
-    review_vip_branch = review_vip.loc[(review_vip['contract_type'] == 'VIP Branch')].copy()
-    # Query RCF3001
-    # setup 6 tháng
-    query_rcf01_6m = pd.read_sql(
+    review_vip['rate'] = review_vip['contract_type'].str.split('Margin.PIA ').str.get(1).str.split('%').str.get(0)
+    review_vip['rate'] = review_vip['rate'].astype(float)
+    mask_phs = (review_vip['current_vip'] == 'GOLD PHS') | (review_vip['current_vip'] == 'SILV PHS')
+    review_vip_phs = review_vip.loc[mask_phs].copy()
+    mask_branch = review_vip['current_vip'] == 'VIP Branch'
+    review_vip_branch = review_vip.loc[mask_branch].copy()
+
+    # month, year of end_date
+    check_date = dt.datetime.strptime(end_date, "%Y-%m-%d")
+    check_month = check_date.month
+    check_year = check_date.year
+
+    if check_month == 6:
+        start_date = dt.datetime(check_year, 1, 1).strftime('%Y-%m-%d')
+        end_date = end_date
+    elif check_month == 12:
+        start_date = dt.datetime(check_year, 7, 1).strftime('%Y-%m-%d')
+        end_date = end_date
+    else:
+        start_date = start_date
+        end_date = end_date
+
+    # query RCF3001
+    query_rcf01 = pd.read_sql(
         f"""
             SELECT
+                [date],
                 [account_code],
-                SUM([trading_fee])[trading_fee],
-                SUM([loan_fee])[loan_fee]
+                [trading_fee],
+                [loan_fee]
             FROM [revenue_from_vip]
             WHERE 
-                [date] BETWEEN '2021-01-01' AND '2021-06-30'
-            GROUP BY [account_code]
+                [date] BETWEEN '{start_date}' AND '{end_date}'
             ORDER BY [account_code]
         """,
         connect_DWH_CoSo,
-        index_col='account_code'
     )
-    query_rcf01_3m = pd.read_sql(
+    # query RCF3002
+    query_rcf02 = pd.read_sql(
         f"""
-            SELECT
-                [account_code],
-                SUM([trading_fee])[trading_fee],
-                SUM([loan_fee])[loan_fee]
-            FROM [revenue_from_vip]
+            SELECT 
+                *
+            FROM [vip_evaluation]
             WHERE 
-                [date] BETWEEN '2021-04-01' AND '2021-06-30'
-            GROUP BY [account_code]
+                [date] BETWEEN '{start_date}' AND '{end_date}'
             ORDER BY [account_code]
         """,
         connect_DWH_CoSo,
-        index_col='account_code'
     )
-    review_vip_phs['trading_fee'] = query_rcf01_6m['trading_fee']
-    review_vip_branch['trading_fee'] = query_rcf01_3m['trading_fee']
+
+    if start_date != sod:
+        start_date_3m = sod
+        # loc RCF3001
+        query_rcf01['date'] = pd.to_datetime(query_rcf01['date'], format='%Y-%m-%d')
+        query_rcf01_3m = query_rcf01.loc[(query_rcf01['date'] >= start_date_3m) and (query_rcf01['date'] <= end_date)]
+        query_rcf01_3m = query_rcf01_3m.groupby('account_code').sum()
+        query_rcf01_6m = query_rcf01.groupby('account_code').sum()
+        # loc RCF3002
+        query_rcf02['date'] = pd.to_datetime(query_rcf02['date'], format='%Y-%m-%d')
+        query_rcf02_3m = query_rcf02.loc[(query_rcf02['date'] >= start_date_3m) and (query_rcf02['date'] <= end_date)]
+        query_rcf02_3m = query_rcf02_3m.groupby('account_code').sum()
+        query_rcf02_6m = query_rcf02.groupby('account_code').sum()
+        # review vip GOLD, SILV 6 months
+        review_vip_phs['trading_fee'] = query_rcf01_6m['trading_fee']
+        # review_vip_phs['tai_san_rong'] = query_rcf02_6m['tai_san_rong']
+        review_vip_phs.fillna(0, inplace=True)
+        # review vip VIP CN 3 months
+        review_vip_branch['trading_fee'] = query_rcf01_3m['trading_fee']
+        # review_vip_branch['tai_san_rong'] = query_rcf02_3m['tai_san_rong']
+        review_vip_branch.fillna(0, inplace=True)
+    else:
+        query_rcf01 = query_rcf01.groupby('account_code').sum()
+        review_vip_branch['trading_fee'] = query_rcf01['trading_fee']
+        # review_vip_branch['tai_san_rong'] = query_rcf02['tai_san_rong']
+        review_vip_branch.fillna(0, inplace=True)
 
     # --------------------- Viet File ---------------------
     # Write file excel Báo cáo review KH vip
@@ -332,7 +347,7 @@ def run(
             'font_size': 8
         }
     )
-    fee_and_value_format = workbook.add_format(
+    money_format = workbook.add_format(
         {
             'border': 1,
             'text_wrap': True,
@@ -340,10 +355,10 @@ def run(
             'valign': 'vcenter',
             'font_name': 'Times New Roman',
             'font_size': 8,
-            'num_format': '(#,##0.0_);_( (#,##0.0);_(* "-"??_);_(@)'
+            'num_format': '#,##0'
         }
     )
-    num_percent_format = workbook.add_format(
+    per_cri_fee_format = workbook.add_format(
         {
             'border': 1,
             'text_wrap': True,
@@ -352,6 +367,17 @@ def run(
             'font_name': 'Times New Roman',
             'font_size': 8,
             'num_format': '0.00"%"'
+        }
+    )
+    rate_format = workbook.add_format(
+        {
+            'border': 1,
+            'text_wrap': True,
+            'align': 'right',
+            'valign': 'vcenter',
+            'font_name': 'Times New Roman',
+            'font_size': 8,
+            'num_format': '0.0"%"'
         }
     )
     footer_format = workbook.add_format(
@@ -385,7 +411,17 @@ def run(
             'font_size': 9,
         }
     )
-    table_content_format =  workbook.add_format(
+    table_empty_format = workbook.add_format(
+        {
+            'border': 1,
+            'text_wrap': True,
+            'align': 'center',
+            'valign': 'top',
+            'font_name': 'Times New Roman',
+            'font_size': 11,
+        }
+    )
+    table_content_format = workbook.add_format(
         {
             'border': 1,
             'text_wrap': True,
@@ -395,7 +431,7 @@ def run(
             'font_size': 11,
         }
     )
-    date_header = dt.datetime.strptime(end_date, "%Y/%m/%d").strftime("%d.%m.%Y")
+    date_header = dt.datetime.strptime(end_date, "%Y-%m-%d").strftime("%d.%m.%Y")
     headers = [
         'No.',
         'Account',
@@ -431,15 +467,15 @@ def run(
     review_vip_sheet.set_column('A:A', 4)
     review_vip_sheet.set_column('B:B', 10.43)
     review_vip_sheet.set_column('C:C', 21.86)
-    review_vip_sheet.set_column('D:D', 9.71)
+    review_vip_sheet.set_column('D:D', 16.71)
     review_vip_sheet.set_column('E:E', 10.86)
     review_vip_sheet.set_column('F:F', 12.43)
     review_vip_sheet.set_column('G:G', 15)
     review_vip_sheet.set_column('H:H', 12.57)
     review_vip_sheet.set_column('I:I', 19.71)
-    review_vip_sheet.set_column('J:J', 21.14)
+    review_vip_sheet.set_column('J:J', 13.3)
     review_vip_sheet.set_column('K:K', 16)
-    review_vip_sheet.set_column('L:L', 12.14)
+    review_vip_sheet.set_column('L:L', 9.14)
     review_vip_sheet.set_column('M:N', 19)
     review_vip_sheet.set_column('O:O', 17.14)
     review_vip_sheet.set_column('P:P', 27.86)
@@ -453,7 +489,7 @@ def run(
 
     # merge row
     review_vip_sheet.merge_range('D2:M2', sheet_title_name, sheet_title_format)
-    report_date = dt.datetime.strptime(end_date, "%Y/%m/%d").strftime("%B, %Y")
+    report_date = dt.datetime.strptime(end_date, "%Y-%m-%d").strftime("%B, %Y")
     review_vip_sheet.merge_range('D3:M3', '', empty_format)
     review_vip_sheet.write_rich_string(
         'D3',
@@ -466,7 +502,7 @@ def run(
 
     review_vip_sheet.write('N2', 'No.:', no_and_date_format)
     review_vip_sheet.write('N3', 'Date:', no_and_date_format)
-    review_vip_sheet.merge_range('O2:P2', '129/2021/TTr-TRS', no_and_date_format)
+    review_vip_sheet.merge_range('O2:P2', f'     /{run_time.year}/TTr-TRS', no_and_date_format)
     review_vip_sheet.merge_range('O3:P3', f'{run_time.strftime("%d/%m/%Y")}', no_and_date_format)
 
     review_vip_sheet.merge_range('A5:P5', description, description_format)
@@ -506,13 +542,101 @@ def run(
     review_vip_sheet.write_column(
         'F7',
         review_vip['criteria_fee'],
-        fee_and_value_format
+        money_format
     )
     review_vip_sheet.write_column(
         'J7',
-        review_vip['contract_type'],
+        review_vip['current_vip'],
         text_left_format
     )
+    review_vip_sheet.write_column(
+        'L7',
+        review_vip['rate'],
+        rate_format
+    )
+    review_vip_sheet.write_column(
+        'M7',
+        [''] * review_vip.shape[0],
+        text_left_format
+    )
+    review_vip_sheet.write_column(
+        'N7',
+        [''] * review_vip.shape[0],
+        text_left_format
+    )
+    review_vip_sheet.write_column(
+        'O7',
+        [''] * review_vip.shape[0],
+        text_left_format
+    )
+    review_vip_sheet.write_column(
+        'P7',
+        [''] * review_vip.shape[0],
+        text_left_format
+    )
+    review_vip_sheet.write_column(
+        'Q7',
+        review_vip['branch_name'],
+        text_left_format
+    )
+    review_vip_sheet.write_column(
+        'R7',
+        [''] * review_vip.shape[0],
+        text_left_format
+    )
+    table_start_row = review_vip.shape[0] + 9
+    footer = 'Effective: Promoted accounts be applied from ......./......./2021 & another accounts be applied from ........../........../2021'
+    review_vip_sheet.merge_range(
+        f'A{table_start_row}:P{table_start_row}',
+        footer,
+        footer_format
+    )
+    review_vip_sheet.merge_range(
+        f'B{table_start_row+2}:P{table_start_row+2}',
+        'TRADING SERVICE DIVISION',
+        table_header_format
+    )
+    review_vip_sheet.merge_range(
+        f'B{table_start_row+3}:H{table_start_row+3}',
+        'PROPOSER',
+        table_sub_header_format
+    )
+    review_vip_sheet.merge_range(
+        f'I{table_start_row + 3}:P{table_start_row + 3}',
+        'DIRECTOR OF TRADING SERVICE DIVISION',
+        table_sub_header_format
+    )
+    review_vip_sheet.merge_range(
+        f'B{table_start_row + 4}:H{table_start_row + 7}',
+        '',
+        table_empty_format
+    )
+    review_vip_sheet.merge_range(
+        f'I{table_start_row + 4}:P{table_start_row + 7}',
+        '',
+        table_empty_format
+    )
+    review_vip_sheet.merge_range(
+        f'B{table_start_row + 9}:H{table_start_row + 9}',
+        'Decision of Deputy General Director:',
+        table_sub_header_format
+    )
+    review_vip_sheet.merge_range(
+        f'I{table_start_row + 9}:P{table_start_row + 9}',
+        'DEPUTY GENERAL DIRECTOR',
+        table_sub_header_format
+    )
+    review_vip_sheet.merge_range(
+        f'B{table_start_row + 10}:H{table_start_row + 14}',
+        "o Agree\no Disagree\no Others:................",
+        table_content_format
+    )
+    review_vip_sheet.merge_range(
+        f'I{table_start_row + 10}:P{table_start_row + 14}',
+        '',
+        table_empty_format
+    )
+
     ###########################################################################
     ###########################################################################
     ###########################################################################
