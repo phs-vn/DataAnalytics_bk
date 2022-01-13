ea0e2168458c7268455a7d660bcc366cbb54d86a
Nam Tran Huy <namtran@phs.vn>
2022-01-13 15:39:37 +0700

    nam-tran-push-new-code-crawl-data-ndh

--- News Filter Project/ndh_scrape_news.py
+++ News Filter Project/ndh_scrape_news.py
@@ -1,5 +1,3 @@
-import time
-import pandas as pd
 from selenium.webdriver.chrome.service import Service
 from phs import *
 
@@ -12,8 +10,11 @@ class PageFailToLoad(Exception):
     pass
 
 
-t = 2
+scroll_pause_time = 1
 max_wait_time = 10
+i = 1
+t = 2
+
 ignored_exceptions = (
     ValueError,
     IndexError,
@@ -26,67 +27,68 @@ ignored_exceptions = (
 
 PATH = r'D:\DataAnalytics\News Filter Project\chromedriver_win32\chromedriver.exe'
 chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
-driver.get('https://ndh.vn/doanh-nghiep')
-scroll_pause_time = 1
+driver.get('https://ndh.vn/tai-chinh')
+
 screen_height = driver.execute_script("return window.screen.height;")
-i = 1
+
+mr_data = pd.read_excel(r'D:\DataAnalytics\News Filter Project\Intranet list_07.01.2022 final.xlsx',
+                        sheet_name='Sheet1', usecols="B")
+margin_list = mr_data['Mã CK'].tolist()
+margin_list = [' {0}'.format(elem) for elem in margin_list]
 
 type_save = []
 title_save = []
 url_save = []
 content = []
-time_save = []
 df = pd.DataFrame()
 
 # infinite scrolling
 while True:
+    elements = driver.find_elements(By.XPATH, '//*[@id="content-list-news"]/article')
     # scroll one screen height each time
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
-    i += 1
     time.sleep(scroll_pause_time)
     # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
     scroll_height = driver.execute_script("return document.body.scrollHeight;")
     # Break the loop when the height we need to scroll to is larger than the total scroll height
+
     if screen_height * i > scroll_height:
         driver.find_element(By.XPATH, '//*[@id="btnLoadmore"]/a').click()
-        elements = driver.find_elements(By.XPATH, '//*[@id="content-list-news"]/article')
-        if len(elements) > 500:
-            break
-        # try:
-        #     elements = driver.find_elements(By.XPATH, '//*[@id="content-list-news"]/article')
-        for ele in elements:
-            titles = (ele.text.split('\n'))[0]
-            news_type = ((ele.text.split('\n'))[1]).split('/')[0]
-            time_news = ((ele.text.split('\n'))[1]).split('/')[1]
-            next_url = ele.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    if len(elements) >= 250:
+        break
+    i += 1
 
-            next_driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
-            next_driver.get(next_url)
-            next_tags = next_driver.find_elements(By.XPATH, '/html/body/section[4]/div[2]/article')
-            paragraphs = [each_next_tag.text for each_next_tag in next_tags]
-            para_listToStr = ' '.join([str(elem) for elem in paragraphs])
+for ele in elements:
+    title = (ele.text.split('\n'))[0]
+    news_type = ((ele.text.split('\n'))[1]).split('/')[0]
+    next_url = ele.find_element(By.TAG_NAME, 'a').get_attribute('href')
 
-            title_save.append(titles)
-            type_save.append(news_type)
-            time_save.append(time_news)
-            url_save.append(next_url)
-            content.append(para_listToStr)
-            time.sleep(t)
+    next_driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
+    next_driver.get(next_url)
+    next_tags = next_driver.find_elements(By.XPATH, '/html/body/section[4]/div[2]/article')
+    paragraphs = [each_next_tag.text for each_next_tag in next_tags]
+    para_listToStr = ' '.join([str(elem) for elem in paragraphs])
+    check = [mr in para_listToStr for mr in margin_list]
 
-            next_driver.quit()
-        dictionary = {
-            'Thời gian': time_save,
-            'Loại tin tức': type_save,
-            'Tiêu đề': title_save,
-            'Link': url_save,
-            'Nội dung': content
-        }
-        df = pd.DataFrame(dictionary)
-        # except ignored_exceptions:
-        #     break
-else:
-    driver.quit()
+    if any(check):
+        print("Add title: ", title, ' + ', 'Length: ', len(content))
+        title_save.append(title)
+        type_save.append(news_type)
+        url_save.append(next_url)
+        content.append(para_listToStr)
+    if len(content) > 40:
+        break
+    time.sleep(t)
 
-df.to_pickle("./News Filter Project/output_data/vietstock_data.pkl")
+dictionary = {
+    'Link': url_save,
+    'Loại tin tức': type_save,
+    'Tiêu đề': title_save,
+    'Nội dung': content
+}
+df = pd.DataFrame(dictionary)
+
+# df.to_pickle("./News Filter Project/output_data/ndh_tai-chinh_data.pickle")
