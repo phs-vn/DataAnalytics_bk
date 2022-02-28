9b2470b3eaf7062a3b5a2d6ce0e91450fc0019f3
Nam Tran Huy <namtran@phs.vn>
2022-02-28 18:09:59 +0700

    namtran-update-code-crawl-data-on-vietstock

--- news_analysis/vietstock_scrape_news.py
+++ news_analysis/vietstock_scrape_news.py
@@ -9,75 +9,72 @@ class PageFailToLoad(Exception):
     pass
 
 
+# PATH = r'D:\DataAnalytics\dependency\chromedriver.exe'
 PATH = join(dirname(dirname(realpath(__file__))), 'dependency', 'chromedriver')
 ignored_exceptions = (
-        ValueError,
-        IndexError,
-        NoSuchElementException,
-        StaleElementReferenceException,
-        TimeoutException,
-        ElementNotInteractableException,
-        PageFailToLoad,
-        ElementClickInterceptedException
-    )
+    ValueError,
+    IndexError,
+    NoSuchElementException,
+    StaleElementReferenceException,
+    TimeoutException,
+    ElementNotInteractableException,
+    PageFailToLoad,
+    ElementClickInterceptedException
+)
 margin_list = internal.mlist()
-t = 2
+t = 1
+
+type_save = []
+title_save = []
+url_save = []
+content = []
+time_save = []
 
 
 def run():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-
+    chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
-    driver.get('https://vietstock.vn/kinh-te.htm')
-
-    type_save = []
-    title_save = []
-    url_save = []
-    content = []
-    time_save = []
+    driver.get('https://vietstock.vn/chung-khoan.htm')
     wait = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions)
-    wait_2 = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions)
 
-    while len(title_save) < 130:
+    while len(title_save) < 150:
         # elements = driver.find_elements(By.XPATH, '//*[@id="channel-container"]/*/div')
         elements = wait.until(
             EC.presence_of_all_elements_located((By.XPATH, '//*[@id="channel-container"]/*/div'))
         )
-        try:
-            for ele in elements:
-                news_type = (ele.text.split('\n'))[0]
-                titles = (ele.text.split('\n'))[1]
-                time_news = (ele.text.split('\n'))[2]
-                next_url = ele.find_element(By.TAG_NAME, 'a').get_attribute('href')
 
-                chrome_options.add_argument("--headless")
-                next_driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
+        for ele in elements:
+            news_type = (ele.text.split('\n'))[0]
+            titles = (ele.text.split('\n'))[1]
+            time_news = (ele.text.split('\n'))[2]
+            next_url = ele.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            next_driver = webdriver.Chrome(options=chrome_options, service=Service(PATH))
+            try:
                 next_driver.get(next_url)
-                next_tags = next_driver.find_elements(By.XPATH, '//*[@id="vst_detail"]/p')
-                paragraphs = [each_next_tag.text for each_next_tag in next_tags]
-                para_listToStr = ' '.join([str(elem) for elem in paragraphs])
-                check = [mr in para_listToStr for mr in margin_list]
-                if any(check):
-                    type_save.append(news_type)
-                    title_save.append(titles)
-                    time_save.append(time_news)
-                    url_save.append(next_url)
-                    content.append(para_listToStr)
-                    print('Title: ', titles, ' + ', 'Length:', len(title_save))
-                time.sleep(t)
-                next_driver.quit()
-        except ignored_exceptions:
-            continue
-        time.sleep(4)
-        try:
-            next_page = wait_2.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#page-next\  > a')))
-            next_page.click()
-        except ignored_exceptions:
-            time.sleep(5)
-            next_page = wait_2.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#page-next\  > a')))
-            next_page.click()
+            except ignored_exceptions:
+                next_driver.refresh()
+            next_tags = next_driver.find_elements(By.XPATH, '//*[@id="vst_detail"]/p')
+            paragraphs = list(filter(None, [each_next_tag.text for each_next_tag in next_tags]))
+            para_listToStr = ' '.join([str(elem) for elem in paragraphs])
+            check = [mr in para_listToStr for mr in margin_list]
+            if any(check):
+                type_save.append(news_type)
+                title_save.append(titles)
+                time_save.append(time_news)
+                url_save.append(next_url)
+                content.append(para_listToStr)
+                print('Title: ', titles, ' + ', 'Length:', len(title_save))
+            time.sleep(t)
+            next_driver.quit()
 
+        while True:
+            try:
+                next_page = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#page-next\  > a')))
+                next_page.click()
+                break
+            except ignored_exceptions:
+                continue
         time.sleep(t)
     else:
         driver.quit()
@@ -90,4 +87,5 @@ def run():
         'Nội dung': content
     }
     df = pd.DataFrame(dictionary)
-    df.to_pickle(r"D:\DataAnalytics\news_analysis\output_data\vietstock_kinh-te_data-2.pickle")
+    df.to_pickle(r"D:\DataAnalytics\news_analysis\output_data\vietstock\vietstock_data_3"
+                 r"\vietstock_chung-khoan_data-3.pickle")
