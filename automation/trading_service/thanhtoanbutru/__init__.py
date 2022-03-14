8394e7fea5f29e500bab14d9c4325501857490f9
phs.vn <hiepdang@phs.vn>
2022-03-14 14:49:39 +0700

    Push 2022-03-14

--- automation/trading_service/thanhtoanbutru/__init__.py
+++ automation/trading_service/thanhtoanbutru/__init__.py
@@ -1,3 +1,36 @@
+import pandas as pd
+
 from automation.trading_service import *
 
 dept_folder = r'C:\Users\hiepdang\Shared Folder\Trading Service\Report\ThanhToanBuTru'
+
+def getBranchBroker(
+    x:str,
+    d:dt.datetime
+) -> pd.Series:
+
+    """
+    This function return branch name and broker name given an account code or sub account
+
+    :param x: either account_code or sub_auccount
+    :param d: date to check the relationship
+    """
+
+    sqlStatement = f"""
+        SELECT DISTINCT
+            [branch].[branch_name], 
+            [broker].[broker_name]
+        FROM [relationship]
+        LEFT JOIN [branch] ON [branch].[branch_id] = [relationship].[branch_id]
+        LEFT JOIN [broker] ON [broker].[broker_id] = [relationship].[broker_id]
+        WHERE [relationship].[date] = '{d.strftime("%Y-%m-%d")}'
+        """
+
+    if re.findall('[A-Z]',x): # account code
+        sqlStatement += f" AND [relationship].[account_code] = '{x}'"
+    else:
+        sqlStatement += f" AND [relationship].[sub_account] = '{x}'"
+
+    print(sqlStatement)
+
+    return pd.read_sql(sqlStatement,connect_DWH_CoSo).squeeze()
